fd003661e7cdc24681b9c680a3c6364b35470583
Petter Walbø Johnsgård <petter@dekode.no>
2021-11-18 00:03:16 +0100

    init

--- src/scripts/build.ts
+++ src/scripts/build.ts
@@ -2,18 +2,54 @@
  * External dependencies
  */
 import { sync as fastGlob } from 'fast-glob';
-import path from 'path';
+import { join } from 'path';
+import { set } from 'lodash';
+import { readFileSync, writeFileSync } from 'fs';
 
 /**
  * Internal dependencies
  */
 import { getCurrentWorkingDirectory } from '../utils';
 
-const root = path.join(
-	getCurrentWorkingDirectory(),
-	'tests',
-	'data',
-	'theme-json',
-);
-const files = fastGlob(path.join(root, '**/*.json'));
-console.log(files);
+async function build() {
+	const root = join(
+		getCurrentWorkingDirectory(),
+		'tests',
+		'data',
+		'theme-json',
+		'/',
+	);
+	const files = fastGlob(join(root, '**/*.json'));
+
+	const themeJson = files.reduce((previousValue, file) => {
+		try {
+			const content = readFileSync(file, {
+				encoding: 'utf-8',
+			});
+			const config = JSON.parse(content);
+
+			const destination = file.replace(root, '').replace('.json', '');
+			const splittedDestination = destination.split('/blocks/');
+
+			let dest = splittedDestination[0].split('/');
+			if (splittedDestination.length !== 1) {
+				dest = [...dest, 'blocks', splittedDestination[1]];
+			}
+
+			set(previousValue, dest, config);
+		} catch (err) {
+			console.log(file, err);
+		}
+
+		return previousValue;
+	}, {});
+
+	writeFileSync(
+		join(getCurrentWorkingDirectory(), 'theme.json'),
+		JSON.stringify(themeJson, null, '\t'),
+	);
+
+	console.log('theme.json created');
+}
+
+build();
